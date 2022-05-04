4d77604f2f445d98870ff8488cd142630d37da04
Ayesh Dulanja <ayeshdulanja@gmail.com>
2022-05-04 16:44:54 +0530

    .

---
+++ src/main/java/com/ad/HelloWorld/Controller.java
@@ -0,0 +1,14 @@
+package com.ad.HelloWorld;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller
+{
+    @GetMapping("/hello")
+    public String hello()
+    {
+        return "Hello World";
+    }
+}
---
+++ src/test/java/com/ad/HelloWorld/HelloWorldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ad.HelloWorld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloWorldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
