a036c1d1a1688224741e3dc7b99f26274349be88
Ayesh Dulanja <ayeshdulanja@gmail.com>
2022-05-05 11:21:09 +0530

    .

--- src/main/java/com/ad/HelloWorld/Controller.java
+++ src/main/java/com/ad/HelloWorld/Controller.java
@@ -1,14 +1,16 @@
 package com.ad.HelloWorld;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Controller
 {
-    @GetMapping("/")
-    public @ResponseBody HelloWorld getMessage()
+    @RequestMapping("/")
+    public HelloWorld getMessage()
+    //public @ResponseBody HelloWorld getMessage()
     {
         HelloWorld hw = new HelloWorld();
         hw.setMessage("Hello World");
